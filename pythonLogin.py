8373a5975f0bfce6560ad3943e3042e640076b17
TharinduBuddhika639 <44730081+TharinduBuddhika639@users.noreply.github.com>
2018-11-16 22:56:52 +0530

    python login

---
+++ pythonLogin.py
@@ -0,0 +1,12 @@
+
+
+userName=input("Enter the user name  : ")
+password =input("Enter the password  : ")
+
+if userName=="user":
+	if password=="user123":
+		print("Login success !!!")
+	else:
+		print("Wrong username or Password")
+else:
+	print("Wrong username or Password")
